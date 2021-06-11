b772f07447c898c29899ac9b1b04bb97b4e38aef
pgora <pgora@paypal.com>
2021-06-11 14:17:43 +0530

    authorization configuration

--- src/main/java/org/example/spring/employee/config/SecurityConfiguration.java
+++ src/main/java/org/example/spring/employee/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.example.spring.employee.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    //configuring authentication in spring security
     @Override
     protected void configure(AuthenticationManagerBuilder builder) throws Exception {
         builder.inMemoryAuthentication()
@@ -26,6 +28,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    //configuring authorization in spring security
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/employees/{emp-id}").hasRole("CEO")
+                .antMatchers("/**").permitAll()
+                .and().formLogin();
     }
 
 }
